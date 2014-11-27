b9c0ea1e72c8efaf40d24781613dd2624fec7d5f
Rohit Yengisetty <rohit@cyngn.com>
2014-11-26 19:18:49 -0800

    Calendar : Adding Year View
    Depends on changes to datetimepicker
    @ http://review.cyanogenmod.org/#/c/79480/
    
    Change-Id: Ie528d8c1753477b94a9c477f5735a198565afd4d

--- Android.mk
+++ Android.mk
@@ -29,6 +29,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
         android-opt-datetimepicker \
         android-opt-timezonepicker \
         android-support-v4 \
+        android-support-v13 \
         calendar-common
 
 LOCAL_SDK_VERSION := current
@@ -40,6 +41,12 @@ LOCAL_PACKAGE_NAME := Calendar
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags \
                              ../../../frameworks/opt/datetimepicker/proguard.flags
 
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    LOCAL_PROGUARD_ENABLED := obfuscation
+else
+    LOCAL_PROGUARD_ENABLED := disabled
+endif
+
 LOCAL_AAPT_FLAGS := --auto-add-overlay
 LOCAL_AAPT_FLAGS += --extra-packages com.android.ex.chips
 LOCAL_AAPT_FLAGS += --extra-packages com.android.colorpicker
@@ -49,4 +56,4 @@ LOCAL_AAPT_FLAGS += --extra-packages com.android.timezonepicker
 include $(BUILD_PACKAGE)
 
 # Use the following include to make our test apk.
-include $(call all-makefiles-under,$(LOCAL_PATH))
+# include $(call all-makefiles-under,$(LOCAL_PATH))
