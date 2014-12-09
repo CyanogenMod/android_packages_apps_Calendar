a91c50a577c9f40267944a2e62d7e5b5489c4f5b
Matt Garnes <matt@cyngn.com>
2014-12-08 17:47:07 -0800

    Support sharing calendar events as .vcs files to MMS
    - Export calendar events to a temporary .vcs file. Currently, the actual
      file format is still ical, this may need to be reformatted to actually
      contain only vCalendar content.
    - Share this specifically to the com.android.mms package when the
      share button is clicked.
    - Share the event as ical .ics to all other applications.
    
    Change-Id: If337360b17d4071d580087ecc97a2397d0563ff6

--- src/com/android/calendar/AllInOneActivity.java
+++ src/com/android/calendar/AllInOneActivity.java
@@ -448,8 +448,8 @@ public class AllInOneActivity extends AbstractCalendarActivity implements EventH
             getLoaderManager().initLoader(0, null, this);
         }
 
-        // Clean up cached ics files - in case onDestroy() didn't run the last time
-        cleanupCachedIcsFiles();
+        // Clean up cached ics and vcs files - in case onDestroy() didn't run the last time
+        cleanupCachedEventFiles();
     }
 
     private long parseViewAction(final Intent intent) {
@@ -641,22 +641,22 @@ public class AllInOneActivity extends AbstractCalendarActivity implements EventH
 
         CalendarController.removeInstance(this);
 
-        // Clean up cached ics files
-        cleanupCachedIcsFiles();
+        // Clean up cached ics and vcs files
+        cleanupCachedEventFiles();
     }
 
     /**
-     * Cleans up the temporarily generated ics files in the cache directory
-     * The files are of the format *.ics
+     * Cleans up the temporarily generated ics and vcs files in the cache directory
+     * The files are of the format *.ics and *.vcs
      */
-    private void cleanupCachedIcsFiles() {
+    private void cleanupCachedEventFiles() {
         if (!isExternalStorageWritable()) return;
         File cacheDir = getExternalCacheDir();
         File[] files = cacheDir.listFiles();
         if (files == null) return;
         for (File file : files) {
             String filename = file.getName();
-            if (filename.endsWith(".ics")) {
+            if (filename.endsWith(".ics") || filename.endsWith(".vcs")) {
                 file.delete();
             }
         }
