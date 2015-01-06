aeeaa60a38b8489a4ad0a433ec9fa3c59b8dab1d
Rohit Yengisetty <rohit@cyngn.com>
2015-01-06 10:20:21 -0800

    Calendar - Added provisions to show/hide Floating Action Button.
    YearView actively shows/hides FAB when the user scrolls to keep all
    the content accessible.
    
    Change-Id: I8005861cb6ecf98758782c034adcc19e30951df8

--- src/com/android/calendar/year/YearViewAdapter.java
+++ src/com/android/calendar/year/YearViewAdapter.java
@@ -26,7 +26,7 @@ public class YearViewAdapter extends BaseAdapter implements View.OnTouchListener
 
     /* Define configuration parameters for a month view */
     // fraction of the view occupied by the header
-    private static final double MONTH_HEADER_HEIGHT = 0.30;
+    private static final double MONTH_HEADER_HEIGHT = 0.25;
 
     // fraction of the header to use for the month title text
     private static final double MONTH_LABEL_FONT_SIZE = MONTH_HEADER_HEIGHT * 0.30;
@@ -35,7 +35,7 @@ public class YearViewAdapter extends BaseAdapter implements View.OnTouchListener
     private static final double PADDING = 0.05;
 
     // fraction of the view to use for text
-    private static final double MONTH_DAY_TEXT_SIZE = (1 - (2 * PADDING)) * (0.7)  / 7;
+    private static final double MONTH_DAY_TEXT_SIZE = (1 - (2 * PADDING)) * (0.5)  / 7;
 
     // The padding b/w the week rows. The container height sans that taken up by the header
     // is equally distributed among six rows
@@ -92,7 +92,7 @@ public class YearViewAdapter extends BaseAdapter implements View.OnTouchListener
         mHeight = height;
         // calculate the dimensions of each of the months
         mMonthWidth = mWidth / mColumns;
-        mMonthHeight = mHeight / (NUMBER_OF_MONTHS / mColumns);
+        mMonthHeight = mMonthWidth;
     }
 
     @Override
